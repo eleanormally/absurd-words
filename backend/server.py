9353122a2978db0888883d0c44eb7a06ae36d147
Kiernan Olson <kiernanolson@gmail.com>
2021-08-29 16:19:33 -0400

    added calculation

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,7 @@ import requests
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
 import os
+import math
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,16 +12,33 @@ CORS(app)
 
 
 def WordsAPIRequest(word):
-    url = "https://wordsapiv1.p.rapidapi.com/words/{}".format(word)
-    headers = {'x-rapidapi-host': "wordsapiv1.p.rapidapi.com"}
+    url = 'https://wordsapiv1.p.rapidapi.com/words/{}'
+    headers = {'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com'}
 
-    key = os.environ["WORDSAPIKEY"]
+    key = os.environ['WORDSAPIKEY']
     headers['x-rapidapi-key'] = key
-    response = requests.request("GET", url, headers=headers)
-    freqResponse = requests.request("GET", url + '/frequency', headers=headers)
-    print(response)
-    print(freqResponse)
+    response = requests.request('GET', url.format(word), headers=headers).json()
+    if(response['success'] == 'false'):
+        return 'Error: ' + response['message']
+    freqResponse = requests.request('GET', url.format(word) + '/frequency', headers=headers).json()
+    defintionLength = len(response['results'][0]['definition'].split())
+    topSynonyms = response['results'][0]['synonyms']
+    perMil = float(freqResponse['frequency']['perMillion'])
+    averagePerMil = 0
+    for s in topSynonyms:
+        sResponse = requests.request('GET', url.format(s) + '/frequency', headers=headers).json()
+        if(response['success'] == 'false'):
+            return 'Synonym Error: ' + response['message']
+        averagePerMil += float(sResponse['frequency']['perMillion'])
+    averagePerMil /= len(topSynonyms)
+    return {
+        'perMil': perMil,
+        'dLength': defintionLength,
+        'sPerMil': averagePerMil
+    }
 
+def calculateScore(perMil, dLength, sPerMil):
+    return sPerMil/(math.log(perMil+1)*dLength)
 class GetWordData(Resource):
     def get(self, word):
         parser = reqparse.RequestParser()
@@ -28,8 +46,12 @@ class GetWordData(Resource):
         args = parser.parse_args()
 
         if args['calculate']:
-            parameters = WordsAPIRequest(word)
-
+            data = WordsAPIRequest(word)
+            if type(data) == str:
+                return '{{error: {message}}}'.format(message=data)
+            calculateScore(data['perMil'], data['dLength'], data['sPerMil'])
+        else:
+            return '{message: "not in database"}'
 
 api.add_resource(GetWordData, '/getWord/<word>')
 
