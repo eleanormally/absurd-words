e82c97ae04d2e91ab145e18d6a141fdfc7af7f2d
Kiernan Olson <kiernanolson@gmail.com>
2021-09-14 00:08:12 -0400

    start frontend

---
+++ frontend/database.js
@@ -0,0 +1,70 @@
+APIURI = "https://absurdwordsbackend.herokuapp.com/";
+
+$(document).ready(function() {
+    
+    getBaseTable();
+
+    $("#logScore").change(function(){
+        ischecked = $(this).is(':checked');
+        if(ischecked){
+            $(".scoreValue").each(function(){
+                $(this).html(Math.round(Math.exp((Number($(this).text())))));
+            });
+        }
+        else {
+            $(".scoreValue").each(function(){
+                $(this).html(Math.log((Number($(this).text()))).toFixed(5));
+            });
+        }
+    });
+
+    $(".searchBox").on('keypress', function (e) {
+        if(e.which === 13) {
+            search($(this).val());
+        }
+    });
+    
+});
+
+function search(word) {
+    $.get(APIURI + "/getWord/" + word.toLowerCase() + "?calculate=true", function(data){
+        if (data.hasOwnProperty("word")) {
+            if($(".showTable").is(":hidden")) {
+                console.log('test');
+                $("#wordTable > tbody").empty();  
+            }
+            row = $(".wordTableBody").append("<tr class=\"wordRow\"></tr>");
+            row.append([
+                $("<td>").html(data['word']),
+                $("<td class=\"scoreValue\">").html(data['score']),
+                $("<td>").html(data['humour']),
+                $("<td>").html(data['ambiguity']),
+                $("<td>").html(data['relatives'])
+            ]);
+        }
+        $(".showTable").show();
+    }
+    );
+}
+
+function getBaseTable() {
+    $.get(APIURI + "/words?sortMethod=scoreInverse", function(data){
+        words = data["results"];
+        words.forEach((word) => {
+            row = $(".wordTableBody").append("<tr class=\"wordRow\"></tr>");
+            row.append([
+                $("<td>").html(word['word']),
+                $("<td class=\"scoreValue\">").html(word['score'].toFixed(5)),
+                $("<td>").html(word['humour'].toFixed(8)),
+                $("<td>").html(word['ambiguity']),
+                $("<td>").html(word['relatives'].toFixed(5))
+            ]);
+        });
+    });
+}
+
+function showTable() {
+    $(".showTable").hide();
+    $(".searchBox").val("");
+    getBaseTable();
+}
